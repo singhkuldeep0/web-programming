f88d148e20394253e240774160cda80c3be6a99e
kuldeep <singhkuldeep7850@gmail.com>
2023-05-25 09:26:50 -0700

    first file on 24-5-2023

---
+++ regis.js
@@ -0,0 +1,41 @@
+// let balance = prompt("Enter your balance");
+
+// if(balance != 0 || typeof(balance)== "number"){
+//    alert("Go to room #5")
+// }
+// else{
+//     let initial = prompt("Enter your initial:");
+//     if((initial >= "A" && initial <= "M") || (initial >="a" && initial<="m")){
+//         alert("Go to Room #12")
+//     }
+//     else if((initial >= "N" && initial <= "R") || (initial >="n" && initial<="r")){
+//          alert("Go to Room #10")   
+//     }
+//     else if((initial >= "S" && initial <= "U") || (initial >="s" && initial<="u")){
+//          alert("Go to Room #8")   
+//     }
+//     else if((initial >= "V" && initial <= "Z") || (initial >="v" && initial<="z")){
+//          alert("Go to Room #8")   
+//     }
+//     else{
+//         alert("invalid value")
+//     }
+// }
+
+
+switch(x){
+    case 2:
+        alert("Two");
+        break;
+    
+    case 3: 
+        alert("Three")
+        break;
+    
+    case 4:
+        alert("four")
+        break;
+
+    default:
+        alert("none")
+}
